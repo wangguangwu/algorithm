66804e971546e2206a658da52a21eec0cd19f6b0
wangguangwu <wangguangwu@pukanghealth.com>
2024-07-24 16:40:46 +0800

    init

---
+++ src/main/java/com/wangguangwu/Main.java
@@ -0,0 +1,11 @@
+package com.wangguangwu;
+
+/**
+ * @author wangguangwu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
